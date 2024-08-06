f7e6c3a5f160bbb445488777885ab3e7c5c8b831
KeisJS <KeisJS@users.noreply.github.com>
2024-08-06 15:47:10 +0300

    T-4: Any agait (#1)
    T-4: any again article

---
+++ src/Typescript/Снова any/code.ts
@@ -0,0 +1,33 @@
+export const anyAgainEx1 = () => {
+  const A: any = 1
+  const B: string = A
+
+  const C = B.repeat(10)
+}
+
+export const anyAgainEx2 = () => {
+  const A: unknown = 1
+  const B: string = typeof A === 'string' ? A : '1'
+
+  return B.repeat(2)
+}
+
+export const anyAgainEx3 = () => {
+  type A<T> = { value: T }
+  type B<T> = T extends any ? A<T> : never
+  type C<T extends { value: any }> = T extends { value: infer InnerT } ? InnerT : never
+
+  type testType = string | number
+
+  type Result = {
+    value1: A<testType> // { value: string | number }
+    value2: B<testType> // { value: string } | { value: number }
+    value3: C<A<string> | B<number>> // string | number
+  }
+
+  const a: Result = {
+    value1: { value: 100 },
+    value2: { value: '100' },
+    value3: 100
+  }
+}
